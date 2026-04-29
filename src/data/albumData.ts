6bba89ed6cfc7f1abbb1fb6c1460f89ce3074722
aull251 <h1101241025@student.untan.ac.id>
2026-04-29 14:30:52 +0700

    feat: add Header component

---
+++ src/data/albumData.ts
@@ -0,0 +1,14 @@
+export const albumData = {
+    type: "Single",
+    title: "You'll Be In My Heart",
+    artist: "NIKI",
+    artistImg: "https://i.scdn.co/image/ab6761610000e5ebb2b3896a88e94e2fd0e25d59",
+    year: "2022",
+    songCount: 2,
+    duration: "8 min 40 sec",
+    coverImg: "https://i.scdn.co/image/ab67616d0000b273f9ea2098e9b96b7e96e1b8f5",
+    tracks: [
+        { number: 1, title: "Before - Acoustic Version", artist: "NIKI", duration: "4:38", explicit: false },
+        { number: 2, title: "You'll Be In My Heart", artist: "NIKI", duration: "4:02", explicit: false },
+    ]
+};
