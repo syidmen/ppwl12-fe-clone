0e7c800d693a238e2dacc9de29ee12efa04df525
syidmen <rasyidmuh0907@gmail.com>
2026-04-29 18:42:09 +0700

    fix: albumData link

--- src/data/albumData.ts
+++ src/data/albumData.ts
@@ -2,11 +2,11 @@ export const albumData = {
     type: "Single",
     title: "You'll Be In My Heart",
     artist: "NIKI",
-    artistImg: "https://i.scdn.co/image/ab6761610000e5ebb2b3896a88e94e2fd0e25d59",
+    artistImg: "https://i.scdn.co/image/ab6761610000101f0654e511623cba4e0e9e2f0c",
     year: "2022",
     songCount: 2,
     duration: "8 min 40 sec",
-    coverImg: "https://i.scdn.co/image/ab67616d0000b273f9ea2098e9b96b7e96e1b8f5",
+    coverImg: "https://i.scdn.co/image/ab67616d0000b2737cd329ea4a204a8a47caf3d5",
     tracks: [
         { number: 1, title: "Before - Acoustic Version", artist: "NIKI", duration: "4:38", explicit: false },
         { number: 2, title: "You'll Be In My Heart", artist: "NIKI", duration: "4:02", explicit: false },
